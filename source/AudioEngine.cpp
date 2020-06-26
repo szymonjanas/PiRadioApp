04e8552dca75ece30453e6a323cece42a6eacaca
szymon <szymon.janas.95@gmail.com>
2020-06-26 18:16:26 +0200

    AudioEngine: changed media status set, removed internal loop

--- source/AudioEngine.cpp
+++ source/AudioEngine.cpp
@@ -8,8 +8,6 @@ AudioEngine::AudioEngine(std::string url){
     Argv[0] = "foo";
     gst_init (&Argc, &Argv);
 
-    loop = g_main_loop_new(NULL, FALSE);
-
     std::string playbinText = "playbin uri=" + url;
     pipeline = gst_parse_launch(playbinText.c_str(), NULL);
 
@@ -22,6 +20,12 @@ AudioEngine::AudioEngine(std::string url){
     bus_watch_id = gst_bus_add_watch(bus, bus_call, loop);
 }
 
+AudioEngine::~AudioEngine(){
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+}
+
 gboolean AudioEngine::bus_call(GstBus *bus, GstMessage *msg, gpointer data){
     GMainLoop *loop = (GMainLoop *) data;
 
@@ -29,7 +33,6 @@ gboolean AudioEngine::bus_call(GstBus *bus, GstMessage *msg, gpointer data){
 
       case GST_MESSAGE_EOS:
         g_print ("End of stream\n");
-        g_main_loop_quit (loop);
         break;
 
       case GST_MESSAGE_ERROR: {
@@ -41,8 +44,6 @@ gboolean AudioEngine::bus_call(GstBus *bus, GstMessage *msg, gpointer data){
 
         g_printerr ("Error: %s\n", error->message);
         g_error_free (error);
-
-        g_main_loop_quit (loop);
         break;
       }
       default:
@@ -52,41 +53,17 @@ gboolean AudioEngine::bus_call(GstBus *bus, GstMessage *msg, gpointer data){
     return TRUE;
 }
 
-bool AudioEngine::setState(AudioState state){
-    switch(state)
-    {
-    case AudioState::PLAY:
-        gst_element_set_state (pipeline, GST_STATE_PLAYING);
-        g_print ("Running...\n");
-        break;
-    case AudioState::PAUSE:
-        gst_element_set_state (pipeline, GST_STATE_PAUSED);
-        g_print ("Paused,\n");
-        break;
-    case AudioState::STOP:
-        g_print ("Returned, stopping playback\n");
-        gst_element_set_state (pipeline, GST_STATE_NULL);
-        break;
-    }
+void AudioEngine::play(){
+    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+    g_print ("Running...\n");
 }
 
+void AudioEngine::pause(){
+    gst_element_set_state (pipeline, GST_STATE_PAUSED);
+    g_print ("Paused,\n");
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void AudioEngine::stop(){
+    g_print ("Returned, stopping playback\n");
+    gst_element_set_state (pipeline, GST_STATE_NULL);
+}
--- source/AudioEngine.hpp
+++ source/AudioEngine.hpp
@@ -7,17 +7,17 @@ class AudioEngine {
 
     GstBus *bus;
     guint bus_watch_id;
-    GMainLoop *loop;
     GstElement *pipeline;
 
     static gboolean bus_call(   GstBus* bus, GstMessage* msg, gpointer data);
 
 public:
 
-    enum AudioState {PLAY, PAUSE, STOP};
-
     AudioEngine(std::string url);
-    bool setState(AudioState state);
+    ~AudioEngine();
+    void play();
+    void pause();
+    void play();
 };
 
 
