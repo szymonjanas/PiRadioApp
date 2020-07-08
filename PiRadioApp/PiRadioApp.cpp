938c16ceb8ea3d67869051295977df22587297e8
szymon <szymon.janas.95@gmail.com>
2020-07-08 20:38:14 +0200

    update main

--- PiRadioApp/PiRadioApp.cpp
+++ PiRadioApp/PiRadioApp.cpp
@@ -5,37 +5,16 @@
 #include "WebEngine.hpp"
 #include <memory>
 
+#include "AudioEngineManager.hpp"
+
 int main(int argc, char ** argv)
 {
+    AudioEngineManager::init();
 
-    Wt::WRun(argc, argv, [](const Wt::WEnvironment& env) {return std::make_unique<WebEngine>(env); });
+    return Wt::WRun(argc, argv, 
+        [](const Wt::WEnvironment& env) {
+            return std::make_unique<WebEngine>(env); 
+        }
+    );
 
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Avaliable command: \n play \n pause \n stop \n title \n exit \n--------" << std::endl;
-    AudioEngine* audio = new AudioEngine("https://stream.open.fm/127?type=.mp3");
-    std::string in;
-    while (true) {
-        std::cin >> in;
-        if (in == "play") audio->play();
-        if (in == "pause") audio->pause();
-        if (in == "stop") audio->stop();
-        if (in == "title") std::cout << audio->getTitle() << std::endl;
-        if (in == "volume") {
-            double volume;
-            std::cin >> volume;
-            audio->setVolume(volume);
-        }
-        if (in == "url") {
-            std::string url;
-            std::cin >> url;
-            if (url != "exit") {
-                delete audio;
-                audio = new AudioEngine(url);
-            }
-        }
-        if (in == "exit") {
-            delete audio;
-            exit(0);
-        }
-    }
 }
