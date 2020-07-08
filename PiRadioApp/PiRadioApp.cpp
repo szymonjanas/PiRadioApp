3010b81697d4e9b74cc4731e8c569369e2c92a19
szymon <szymon.janas.95@gmail.com>
2020-07-08 22:57:09 +0200

    update main

--- PiRadioApp/PiRadioApp.cpp
+++ PiRadioApp/PiRadioApp.cpp
@@ -6,10 +6,12 @@
 #include <memory>
 
 #include "AudioEngineManager.hpp"
+#include "DatabaseEngine.hpp"
 
 int main(int argc, char ** argv)
 {
     AudioEngineManager::init();
+    DatabaseEngine::init();
 
     return Wt::WRun(argc, argv, 
         [](const Wt::WEnvironment& env) {
