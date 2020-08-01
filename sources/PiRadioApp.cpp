35b425389994162911efb708311b138cff5542e4
szymon <szymon.janas.95@gmail.com>
2020-08-01 22:57:17 +0200

    added exec for server golang app

--- sources/PiRadioApp.cpp
+++ sources/PiRadioApp.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
-
 #include "Communication.hpp"
 
 int main(int argc, char ** argv)
 {
     Communication comm;
     comm.bind("tcp://*:5555");
+
+    system("go run ../server/server.go &");
+
     comm.run(new StationsDatabaseTxt("../database.txt"));
 }
