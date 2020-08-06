632f657d1ab68f207e8b3899b2779008d26f2d77
szymon <szymon.janas.95@gmail.com>
2020-08-06 19:13:30 +0200

    changed folder name

---
+++ engine/AudioEngine/AudioEngineManager.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+
+#include "AudioEngine.hpp"
+#include "Station.hpp"
+
+class AudioEngineManager {
+
+	AudioEngine engine;
+	Station* station = nullptr;
+
+	AudioEngineManager();
+
+	void play();
+	void pause();
+	void stop();
+
+public: 
+
+	AudioEngineManager (AudioEngineManager const &) = delete;
+	void operator= (AudioEngineManager const &) = delete;
+
+	static AudioEngineManager& getManager();
+
+	void setStation(Station* station);
+	Station* getStation();
+
+	std::string getState();
+	std::string getDetails(std::string type);
+
+	void setState(std::string state);
+	bool canSetState(std::string state);
+
+	std::string toString();
+};
---
+++ engine/DatabaseEngine/Station.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+
+class Station {
+
+    std::string name = "";
+    std::string uri = "";
+
+    public:
+
+        Station(std::string name, std::string uri);
+
+        std::string getName();
+        std::string getUri();
+
+        void setName(std::string name);
+        void setUri(std::string uri);
+
+        std::string toString();
+
+        static bool check (Station* station);
+        static std::string getString(Station* station);
+        static std::string getName(Station* station);
+        static std::string getUri(Station* station);
+};
