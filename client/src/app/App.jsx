8563860da67ddf3cbf199cbedc296b093506f4d5
Piotr Lewandowski <contact@piotrlewandowski.info>
2020-09-07 16:22:26 +0100

    Add player layout

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import { Header } from './components/Header';
+import { Player } from './components/Player';
 
 const App = () => {
   return (
     <div className="site-wrapper">
       <Header />
+      <main role="main" className="site-content">
+        <Player />
+      </main>
     </div>
   );
 };
---
+++ client/src/app/components/Player.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Player = () => {
+  return (
+    <div className="player-wrapper">
+    </div>
+  );
+};
+
+export { Player };
