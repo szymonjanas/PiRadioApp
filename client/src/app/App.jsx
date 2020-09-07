18652a6f322a3fa6806a30b864a4e88c673a772d
Piotr Lewandowski <contact@piotrlewandowski.info>
2020-09-07 16:12:07 +0100

    Add header

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { Header } from './components/Header';
+
 const App = () => {
   return (
     <div className="site-wrapper">
-      <h1>PiRadioApp</h1>
+      <Header />
     </div>
   );
 };
---
+++ client/src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="heading">PiRadio</h1>
+    </header>
+  );
+};
+
+export { Header };
