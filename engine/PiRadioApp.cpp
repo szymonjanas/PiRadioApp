851037eb60dae43aad57cdfb4c127bf3b0faafc8
szymon <szymon.janas.95@gmail.com>
2020-09-19 21:33:07 +0200

    implement help as json file

--- engine/PiRadioApp.cpp
+++ engine/PiRadioApp.cpp
@@ -8,7 +8,7 @@
 #include "Audio.hpp"
 #include "Logs.hpp"
 
-std::string help();
+void help(std::string);
 
 int main(int argc, char **argv)
 {
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
         if (args[i] == "--help" or args[i] == "-h")
         {
             helpFlag = true;
-            std::cout << help();
+            help("../help.json");
         }
         else if ((args[i] == "--icomm-address" or args[i] == "-ic") && i + 1 < argvc)
         {
@@ -113,38 +113,26 @@ int main(int argc, char **argv)
     return 0;
 }
 
-std::string help() 
+void help(std::string helpPath)
 {
-    std::string helpStr = "";
-    helpStr += "PiRadio help sheet\n";
-    helpStr += "\n";
-    helpStr += " --icomm-address [ip:port]  Internal (server <-> engine) tcp communication address. \n";
-    helpStr += "    -ic [ip:port]               Default: localhost:5555\n";
-    helpStr += " --icomm-port [port]        Internal (server <-> engine) tcp communication port. \n";
-    helpStr += "    -icp [port]                 Default: 5555 \n";
-    helpStr += "\n";
-    helpStr += " --host-address [ip:port]   Host site address.  \n";
-    helpStr += "    -h [ip:port]                Default: localhost:8080\n";
-    helpStr += " --host-port [port]         Host site port.  \n";
-    helpStr += "    -hp [port]                  Default: 8080\n";
-    helpStr += "\n";
-    helpStr += " --resource [path]          Resource path.  \n";
-    helpStr += "    -res [path]                 Default: ../server/resources/\n";
-    helpStr += "\n";
-    helpStr += " --debug                    Debug mode \n";
-    helpStr += "    -d \n";
-    helpStr += "\n";
-    helpStr += " --only                     Run only engine app \n";
-    helpStr += " --only-with-console        Run only engine app with cmd control instead of internal communication tcp  \n";
-    helpStr += "                                Mode for testing purpose \n";
-    helpStr += "\n";
-    helpStr += " --database [path]          Set database path \n";
-    helpStr += "    -db [path] \n";
-    helpStr += "\n";
-    helpStr += " --cmd-colors               Enable colors of console message \n";
-    helpStr += "       -col \n";
-    helpStr += " --basic-cmd                Disable info and debug message.\n";
-    helpStr += "    -b \n";
-    helpStr += "\n \n";
-    return helpStr;
+    if (!helpPath.size()){
+        Log::err("Help file path is empty!");
+        throw std::string("Help file path is empty!");
+    }
+    std::fstream helpFile(helpPath, std::ios::in);
+    if (!helpFile.good()){
+        Log::err("Help file cannot be open: please check if json file exist!");
+        throw std::string("Help file cannot be open: please check if json file exist!");
+    }
+    nlohmann::json dbJson = nlohmann::json::parse(helpFile);
+    helpFile.close();
+
+    auto stations = dbJson["help"];
+    for (auto iter = stations.begin(); iter != stations.end(); ++iter)
+        std::cout 
+                << "  " << (*iter)["set"].get<std::string>() << "\n" 
+                << "   " << (*iter)["short"].get<std::string>() << (((*iter)["short"].get<std::string>()).size() == 0 ? "" : "\n") 
+                << "     " << (*iter)["description"].get<std::string>() << "\n"
+                << "\n";
+
 }
