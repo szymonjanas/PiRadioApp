4c1434980834f5227c0b791bedd3efd6332af40c
szymon <szymon.janas.95@gmail.com>
2020-10-12 21:11:28 +0200

    set travis for run ipc tests

--- tests/engine-ipc-test/__main__.py
+++ tests/engine-ipc-test/__main__.py
@@ -1,18 +1,32 @@
 import test
-import sys
+import sys, time, subprocess
 
 if __name__ == "__main__":
     print("Test running...")
     testsFlag = True
+    engineFlag = False
+
     for arg in sys.argv:
         if arg == "--debug":
             test.debugFlag = True
         if arg == "--ipc-debug":
             test.ipcFlag = True
         if arg == "--help" or arg == "-h":
-            print("Help: [--debug] [--ipc-debug] [--help]")
+            print("Help: [--debug] [--ipc-debug] [--help] [--basic] [--engine]")
             testsFlag = False
-        if arg == "--basic" or arg == "-b":
+        if arg == "--basic":
             test.basicFlag = True
+        if arg == "--engine" or arg == "-e":
+            engineFlag = True
+
     if testsFlag:
+
+        if engineFlag:
+            pidEngine = subprocess.Popen(['build/PiRadioApp', '--only', '-na', '-col', '-db', 'test-database.json'])
+            time.sleep(3)
+        
         test.test()
+
+        if engineFlag:
+            pidEngine.terminate()
+            subprocess.call(['rm', 'piradio.app']) 
