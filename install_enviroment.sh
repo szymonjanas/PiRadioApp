4757f15655d85c7d4f4042064b906393e5c0ca77
Szymon <janas.szymon@outlook.com>
2020-10-25 21:00:57 +0100

    add install dev enviroment script

---
+++ install_enviroment.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "C++ pack install"
+echo ""
+apt-get install -y g++ gcc cmake
+echo ""
+echo ""
+
+echo "Gstreamer"
+echo ""
+apt-get install -y libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-pulseaudio
+echo ""
+echo ""
+
+echo "ZMQ"
+apt-get install -y libzmq5 libzmq3-dev
+echo ""
+echo ""
+
+echo "Golang"
+apt-get install -y golang
+go get github.com/pebbe/zmq4
+echo ""
+echo ""
+
+echo "Python3"
+echo ""
+apt-get install -y python3 python3-pip
+pip3 install termcolor pyzmq
+echo ""
+echo ""
+
+echo "DONE! ENVIROMENT INSTALLED." 
