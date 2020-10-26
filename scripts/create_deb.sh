8bbaa2e4c5db88c655e39f1744efd1557fa81287
Szymon <janas.szymon@outlook.com>
2020-10-26 20:21:29 +0100

    vm test create deb

--- scripts/create_deb.sh
+++ scripts/create_deb.sh
@@ -6,19 +6,20 @@ cd ..
 echo "Assembly deb package..."
 echo ""
 mkdir -v -p linuxconfig/DEBIAN
-cp -v control linuxconfig/DEBIAN
 mkdir -v -p linuxconfig/usr/bin/piradio
-cd -v build
+cp -v control linuxconfig/DEBIAN
+cp -v help.json linuxconfig/usr/bin/piradio
+cp -v -r server/resources linuxconfig/usr/bin/piradio
+
+cd build
 cp -v PiRadioApp ../linuxconfig/usr/bin/piradio
 cp -v libPiRadioAppEngineLib.so ../linuxconfig/usr/bin/piradio
 cp -v server ../linuxconfig/usr/bin/piradio
 cd -v ..
-cp -v help.json linuxconfig/usr/bin/piradio
-cp -v -r server/resources linuxconfig/usr/bin/piradio
 
 dpkg-deb -v --build linuxconfig
 mkdir -v out
-mv -v linuxconfig.deb out/PiRadioApp-1.0.deb
+mv -v linuxconfig.deb out/PiRadioApp-0.6.deb
 echo ""
 echo "Created! You can find out deb package in directory '/out' "
 echo ""
