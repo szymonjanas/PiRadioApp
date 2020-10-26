15cd39247e0270554eca565196e99223d4c51623
Szymon <janas.szymon@outlook.com>
2020-10-26 21:00:31 +0100

    deb add service, run

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-PiRadio=/home/$USER/piradio/
-$PiRadio/PiRadioApp -col -res $PiRadio/resources -db $PiRadio/database/radio-stations.json -rsc $PiRadio/server
+PiRadio=/usr/bin/piradio
+
+$PiRadio/PiRadioApp \ 
+    -res $PiRadio/resources \
+    -rsc $PiRadio/server \
+    -db $PiRadio/database/radio-stations.json \
+    -sha 127.0.0.1:7983
--- scripts/create_deb.sh
+++ scripts/create_deb.sh
@@ -1,5 +1,7 @@
 echo "Creating deb package..."
 
+VER=v0.6
+
 sh build.sh
 cd ..
 
@@ -15,11 +17,15 @@ cd build
 cp -v PiRadioApp ../linuxconfig/usr/bin/piradio
 cp -v libPiRadioAppEngineLib.so ../linuxconfig/usr/bin/piradio
 cp -v server ../linuxconfig/usr/bin/piradio
+cp -v run.sh ../linuxconfig/usr/bin/piradio
 cd ..
+
+mkdir -v -p linuxconfig/etc/systemd/system
+cp -v piradio.service linuxconfig/etc/systemd/system
 
 dpkg-deb -v --build linuxconfig
 mkdir -v out
-mv -v linuxconfig.deb out/PiRadioApp-0.6.deb
+mv -v linuxconfig.deb out/PiRadioApp-$VER.deb
 echo ""
 echo "Created! You can find out deb package in directory '/out' "
 echo ""
