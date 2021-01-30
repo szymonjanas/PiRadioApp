82f7a60cb9e32bb479eca0013385ced06f6ad2dd
Szymon <janas.szymon@outlook.com>
2021-01-30 13:16:31 +0100

    upgrade uninstall script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,7 +1,18 @@
 echo "Uninstall application"
+echo "sudo required!"
 
+
+echo "Stopping piradio.service!"
+systemctl stop piradio.service
+
+echo "Remove piradio.service"
 rm -fv /etc/systemd/system/piradio.service
 
+echo "remove piradio.conf"
 rm -fv /etc/ld.so.conf.d/piradio.conf
 
+echo "remove PiRadioApp"
 rm -rfv /opt/PiRadioApp
+
+echo "Please restart daemon with command: sudo systemctl daemon-reload"
+echo "or stop service with"
